26be3b3531e5a1082e966e9d857a1b2a3de12686
yanghyeyeon <yhy4058@gmail.com>
2024-06-13 13:06:20 +0900

    view

--- 16_VIEW.sql
+++ 16_VIEW.sql
@@ -23,3 +23,59 @@ select * from hansik;
 -- 베이스 테이블(tbl_menu) 정보가 변경되면 VIEW의 결과도 같이 변경된다.
 insert into tbl_menu values (null, '식혜맛국밥', 5500, 4, 'Y');
 select * from hansik;
+
+
+-- VIEW를 통해 데이터를 넣기
+insert into hansik values (null, '수정과맛국밥', 5500 , 4, 'Y');
+
+-- VIEW를 통한 UPDATE
+update hansik
+set
+    menu_name = '버터맛국밥', menu_price = 5700
+where menu_code = 23;
+
+select * from tbl_menu;
+
+-- VIEW를 통한 DELETE
+delete from hansik
+where
+    menu_code = 23;
+
+-- 사용된 SUBQUERY에 따라 명령어로 조작이 불가능한 경우
+-- 1) 뷰 정의에 포함되지 않은 컬럼을 조작하는 경우
+-- 2) 뷰에 포함되지 않는 컬럼 중에 베이스가 되는 테이블 컬럼이 NOT NULl 제약조건에 지정된 경우
+-- 3) 산술 표현식이 정의된 경우
+-- 4) JOIN을 이용해 여러 테이블을 연결한 경우
+-- 5) DISTINCT를 포함한 경우
+-- 6) 그룹함수나 GROUP BY를 포함한 경우
+
+-- VIEW 삭제
+drop view hansik;
+
+-- VIEW에 쓰인 SUBQUERY 연산 결과도 컬럼으로 사용 가능하다.
+create view hansik as
+    select
+        menu_name as '메뉴명',
+        truncate(menu_price / 1000, 1) as '가격(천원)',
+        category_name as '카테고리명'
+from tbl_menu a
+join tbl_category b
+on
+    a.category_code = b.category_code
+where b.category_name = '한식';
+
+select * from hansik;
+
+-- or replace 옵션
+-- 테이블을 drop하지 않고 기존의 view를 새로운 view로 쉽게 교체가능
+create or replace view hansik as
+    select
+        menu_code as '메뉴코드',
+        menu_name as '메뉴명',
+        category_name as '카테고리명'
+from
+    tbl_menu a
+join
+        tbl_category b
+on a.category_code = b.category_code
+where b.category_code  = '한식';
